c12292a85fbd8fefd2870924565d8ff26f04885a
Daniel Baumann <143436969+danielbaumannbr@users.noreply.github.com>
2025-12-05 13:43:32 -0300

    Update docs build artifacts and app files
    Regenerated browser build artifacts in docs, including renaming main JS file and updating index.html to reference it. Removed obsolete files from docs. Updated src/app/app.html and src/app/app.ts with new changes.

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,7 +5,8 @@ import { Imc } from './imc/imc'; // importa o componente standalone Imc
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',   // <- arquivo app.html
+  styleUrls: ['./app.css'],   // <- CSS carregado automaticamente,
   standalone: true,
   imports: [CommonModule, Imc] // garante que <app-imc> é conhecido
 })
-export class App {}
+export class App { }
